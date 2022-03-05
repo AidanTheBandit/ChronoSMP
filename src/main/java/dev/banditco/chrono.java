567a7981a6916da9c29c7e119eac38c30e0324d3
AidanTheBandit <84136868+AidanTheBandit1@users.noreply.github.com>
2022-03-05 02:22:46 -0500

    This version is broken but im going to sleep so feel free to tinker with the code!

--- src/main/java/dev/banditco/chrono.java
+++ src/main/java/dev/banditco/chrono.java
@@ -1,31 +1,17 @@
 package dev.banditco;
 
+import dev.banditco.items.ItemManager;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import static org.bukkit.Registry.ENCHANTMENT;
 
-public final class chrono extends JavaPlugin {
+public final class chrono extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        ItemStack item = new ItemStack(Material.CLOCK);
-        ItemMeta meta = item.getItemMeta();
-        assert meta != null;
-        meta.setDisplayName("ChronoMeter");
-        item.setItemMeta(meta);
-        NamespacedKey key = new NamespacedKey(this,"chronometer");
-        ShapedRecipe recipe = new ShapedRecipe(key, item);
-        recipe.shape(" G ", "GCG", " G ");
-        recipe.setIngredient('G', Material.GOLD_INGOT);
-        recipe.setIngredient('C', Material.CLOCK);
-        Bukkit.addRecipe(recipe);
-        //item.addEnchantment(ENCHANTMENT., 1);
+        Bukkit.getPluginManager().registerEvents(this, this);
+        ItemManager.init();
     }
 
     @Override
