794181a454bcb01974b3e21a10c0c44886ec3704
AidanTheBandit <84136868+AidanTheBandit1@users.noreply.github.com>
2022-03-26 17:37:05 -0400

    Plugin works

--- src/main/java/dev/banditco/chrono.java
+++ src/main/java/dev/banditco/chrono.java
@@ -2,6 +2,7 @@ package dev.banditco;
 
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
@@ -15,10 +16,12 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 
 public final class chrono extends JavaPlugin implements Listener {
-    public HashMap<String, Long> cooldowns = new HashMap<String, Long>();
 
     @Override
     public void onEnable() {
@@ -31,25 +34,18 @@ public final class chrono extends JavaPlugin implements Listener {
         Player p = e.getPlayer();
         int secondsPlayed = p.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
         ItemStack item = p.getItemInHand();
+        UUID uuid = p.getUniqueId();
+
+
 
         if (item == null || item.getType() == Material.AIR)
             return;
-        if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+        if (e.getAction() == Action.RIGHT_CLICK_AIR && p.getCooldown(Material.CLOCK) == 0 || e.getAction() == Action.RIGHT_CLICK_BLOCK && p.getCooldown(Material.CLOCK) == 0) {
             if (item.getType() == Material.CLOCK) {
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, secondsPlayed, 2));
-                p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, secondsPlayed, 2));
-                p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, secondsPlayed, 2));
-                int cooldownTime = 60; // Get number of seconds from wherever you want
-                if(cooldowns.containsKey(p.getName())) {
-                    long secondsLeft = ((cooldowns.get(p.getName())/1000)+cooldownTime) - (System.currentTimeMillis()/1000);
-                    if(secondsLeft>0) {
-                        // Still cooling down
-                        p.sendMessage("You cant use that commands for another "+ secondsLeft +" seconds!");
-                    }
-                }
-                // No cooldown found or cooldown has expired, save new cooldown
-                cooldowns.put(p.getName(), System.currentTimeMillis());
-                // Do Command Here
+                p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, secondsPlayed, 1));
+                p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, secondsPlayed, 1));
+                p.setCooldown(Material.CLOCK, secondsPlayed + 60);
             }
         }
     }
@@ -63,22 +59,12 @@ public final class chrono extends JavaPlugin implements Listener {
 
         if(item == null || item.getType() == Material.AIR)
             return;
-        if(e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
+        if(e.getAction() == Action.LEFT_CLICK_AIR && p.getCooldown(Material.CLOCK) == 0 || e.getAction() == Action.LEFT_CLICK_BLOCK && p.getCooldown(Material.CLOCK) == 0) {
             if(item.getType() == Material.CLOCK) {
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, secondsPlayed, 1));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, secondsPlayed, 1));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, secondsPlayed, 1));
-                int cooldownTime = 60; // Get number of seconds from wherever you want
-                if(cooldowns.containsKey(p.getName())) {
-                    long secondsLeft = ((cooldowns.get(p.getName())/1000)+cooldownTime) - (System.currentTimeMillis()/1000);
-                    if(secondsLeft>0) {
-                        // Still cooling down
-                        p.sendMessage("You cant use that commands for another "+ secondsLeft +" seconds!");
-                    }
-                }
-                // No cooldown found or cooldown has expired, save new cooldown
-                cooldowns.put(p.getName(), System.currentTimeMillis());
-                // Do Command Here
+                p.setCooldown(Material.CLOCK, secondsPlayed + 60);
             }
         }
     }
