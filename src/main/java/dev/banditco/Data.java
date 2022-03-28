95bdedd616afef2653207fb8c0c0c5b4d00489b8
AidanTheBandit <84136868+AidanTheBandit1@users.noreply.github.com>
2022-03-28 19:39:55 -0400

    Latest

--- src/main/java/dev/banditco/Data.java
+++ src/main/java/dev/banditco/Data.java
@@ -1,26 +1,65 @@
 package dev.banditco;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 
-public class Data implements Listener {
+public class Data extends JavaPlugin implements Listener {
+
+
 
     @EventHandler
-    public void onPlayerKilled(PlayerDeathEvent e) {
+    public void onPlayerKilled(PlayerEvent e) {
 
-        Player p = e.getEntity();
+        Player p = e.getPlayer();
+
+        if(p.isDead()) {
+            int secondsPlayed = p.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
+            int sum = secondsPlayed - 5;
+            //p.setStatistic(Statistic.PLAY_ONE_MINUTE());
+        }
+
+        /*
+        Player p = e.getPlayer();
         int deaths = p.getStatistic(Statistic.DEATHS);
-        
+
+        File file = new File(getDataFolder(),"");
+        if(!file.exists()){
+            try{
+                file.createNewFile();
+            }catch(IOException ea){
+                System.out.println("Can't load file! Ooops!");
+                return;
+            }
+        }
+        YamlConfiguration modifyFile = YamlConfiguration.loadConfiguration(file);
+        modifyFile.set("UUID",p.getUniqueId());
+        modifyFile.set("Deaths",deaths);
+
+        try{
+            modifyFile.save(file);
+        }catch (IOException ea){
+            System.out.println("Couldn't edit file!");
+            return;
+        }
 
 
         if (p.getKiller() instanceof Player) {
             Player killer = p.getKiller();
 
         }
+*/
     }
 
 
--- src/main/java/dev/banditco/chrono.java
+++ src/main/java/dev/banditco/chrono.java
@@ -1,9 +1,6 @@
 package dev.banditco;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.Statistic;
+import org.bukkit.*;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,6 +14,7 @@ import org.bukkit.potion.PotionEffectType;
 
 import java.io.*;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 
 public final class chrono extends JavaPlugin implements Listener {
@@ -24,25 +22,10 @@ public final class chrono extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        File file = new File(getDataFolder(),"");
-        if(!file.exists()){
-            try{
-                file.createNewFile();
-            }catch(IOException e){
-                System.out.println("Can't load file! Ooops!");
-                return;
-            }
-        }
-        YamlConfiguration modifyFile = YamlConfiguration.loadConfiguration(file);
-        modifyFile.set("Deaths","");
 
-        try{
-            modifyFile.save(file);
-        }catch (IOException e){
-            System.out.println("Couldn't edit file!");
-            return;
-        }
+
     }
+
     //Checks For Clock Right Click
     @EventHandler
     public void onBoopClock(PlayerInteractEvent e) {
@@ -58,10 +41,40 @@ public final class chrono extends JavaPlugin implements Listener {
             return;
         if (e.getAction() == Action.RIGHT_CLICK_AIR && p.getCooldown(Material.CLOCK) == 0 || e.getAction() == Action.RIGHT_CLICK_BLOCK && p.getCooldown(Material.CLOCK) == 0) {
             if (item.getType() == Material.CLOCK) {
+                int mili = 100;
+                p.setNoDamageTicks(1);
+                p.setCooldown(Material.CLOCK, secondsPlayed - 20);
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, secondsPlayed, 2));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, secondsPlayed, 1));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, secondsPlayed, 1));
-                p.setCooldown(Material.CLOCK, secondsPlayed + 60);
+                p.removePotionEffect(PotionEffectType.SLOW);
+                p.removePotionEffect(PotionEffectType.SLOW_DIGGING);
+                p.removePotionEffect(PotionEffectType.SLOW_FALLING);
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 1);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 2);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 2);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 3);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 5);
             }
         }
     }
@@ -77,10 +90,52 @@ public final class chrono extends JavaPlugin implements Listener {
             return;
         if(e.getAction() == Action.LEFT_CLICK_AIR && p.getCooldown(Material.CLOCK) == 0 || e.getAction() == Action.LEFT_CLICK_BLOCK && p.getCooldown(Material.CLOCK) == 0) {
             if(item.getType() == Material.CLOCK) {
+                int mili = 100;
+                p.setNoDamageTicks(6);
+                p.setCooldown(Material.CLOCK, secondsPlayed - 20);
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, secondsPlayed, 1));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, secondsPlayed, 1));
                 p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, secondsPlayed, 1));
-                p.setCooldown(Material.CLOCK, secondsPlayed + 60);
+                p.removePotionEffect(PotionEffectType.SPEED);
+                p.removePotionEffect(PotionEffectType.JUMP);
+                p.removePotionEffect(PotionEffectType.FAST_DIGGING);
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 1);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 2);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 2);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 1);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 1);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 0);
+                try {
+                    Thread.sleep(mili);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HAT, 10, 0);
             }
         }
     }
